0691bb86ed5b0e1e85ad484ee6d5d05fe4cab80d
Muhammad Sair <msair565@gmail.com>
2024-02-24 14:03:06 +0500

    Revive

--- ArraysSorting/src/App.java
+++ ArraysSorting/src/App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         SelectionSort sr = new SelectionSort();
+        InsertionSort ir = new InsertionSort();
         int arr[] = { 1, 4, 5, 6, 3, 2 };
         // sr.printArr(arr);
-        sr.sort(arr);
+        // sr.sort(arr);
+        ir.sort(arr);
         sr.printArr(arr);
         System.out.println("Hello, World!");
     }
@@ -29,6 +31,21 @@ class SelectionSort {
     void printArr(int ar[]) {
         for (int i = 0; i < ar.length; i++) {
             System.out.println(ar[i] + "");
+        }
+    }
+}
+
+// {6,3,4,2}
+class InsertionSort {
+    void sort(int arr[]) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            int key = i;
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
     }
 }
